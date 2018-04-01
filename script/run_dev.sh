ac05af767062670e57659dc76689aa7b6b901305
jiro4989 <jiroron666@gmail.com>
2018-04-02 00:26:58 +0900

    新しい画像ようの設定に更新。

--- script/run_dev.sh
+++ script/run_dev.sh
@@ -2,8 +2,8 @@
 
 set -eu
 
-find img/actor001/stands/right/ -type f |
-	go run main.go version.go commands.go scale -s 100 |
-	go run main.go version.go commands.go trim -x 40 -y 320 |
+go run main.go version.go commands.go generate |
+	go run main.go version.go commands.go scale -s 50 |
+	go run main.go version.go commands.go trim -x 100 -y 290 |
   sort |
 	go run main.go version.go commands.go paste
