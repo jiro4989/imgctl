5993edea6c88c6d076b86385b159775a8afe4032
jiro <jiroron666@gmail.com>
2018-04-29 17:57:16 +0900

    不要なスクリプの削除とリネーム

--- script/run_dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-go run ./cmd/tkimgutil.go ./cmd/version.go ./cmd/commands.go generate |
-	go run ./cmd/tkimgutil.go ./cmd/version.go ./cmd/commands.go scale -s 50 |
-	go run ./cmd/tkimgutil.go ./cmd/version.go ./cmd/commands.go trim -x 100 -y 290 |
-  sort |
-	go run ./cmd/tkimgutil.go ./cmd/version.go ./cmd/commands.go paste
